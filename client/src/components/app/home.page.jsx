de06d28bffe1c53759b531cd6cc6afe0b364c903
Vincent Daniel <vincent.daniel@soprasteria.com>
2017-03-13 09:35:05 +0100

    fix: Fixing due date input and colors in dropdown options

--- client/src/components/app/home.page.jsx
+++ client/src/components/app/home.page.jsx
@@ -13,6 +13,8 @@ class HomeComponent extends React.Component {
     const { isAuthenticated, redirect } = props;
     if (isAuthenticated) {
       redirect('/projects');
+    } else {
+      document.title = 'D.A.D - Login';
     }
   }
 

05fbadbd4dd17f842030fe8846c979c64c4094b4
Vincent Daniel <vincent.daniel@soprasteria.com>
2017-05-11 17:31:37 +0200

    feat: Updating populate-functional-services.sh with services field

--- bin/populate-functional-services.sh
+++ bin/populate-functional-services.sh
@@ -12,6 +12,7 @@
 # - Name: the name of the functional service to insert
 # - Package: the package of the functional service (ie. Build, OPS, etc.)
 # - Position: the relative position of the package on display (a service with position "10" will be displayed before another service with position "20")
+# - services: technical services comma separated (eg. jenkins,gitlabci,tfs)
 
 set -u
 
@@ -21,8 +22,21 @@ FILE=${3:-functional-services.csv}
 
 (
     IFS=$';\n'
-    while read -r package name position; do
-        fs=$(printf '{"name": "%s", "package": "%s", "position": %s}' "$name" "$package" "$position")
+    while read -r package name position services; do
+        IFS=',' read -r -a servicesArray <<< "$services"
+        if [ ${#servicesArray[@]} -eq 0 ]; then
+            services="[]"
+        else
+            # transforms "service1,service2,service3" into ["service1","service2","service3"]
+            services="["
+            for element in "${servicesArray[@]}"
+            do
+                services="$services\"$element\","
+            done
+            services="${services::-1}]" # ${string::-1} removes the last character as of bash 4.2
+        fi
+
+        fs=$(printf '{"name": "%s", "package": "%s", "position": %s, "services": %s}' "$name" "$package" "$position" "$services")
         echo "Sending: $fs"
         curl -sH "Authorization:Bearer $DAD_JWT_TOKEN" -H 'Content-Type: application/json;charset=UTF-8' -d "$fs" "$DAD_URL/api/services/new"
         echo
